c1b344f537ac7422e30ef52ee56f0d8afb6548c9
Alessandro <alessandrogalindo7@gmail.com>
2019-10-16 21:43:07 -0600

    Pokemon App

---
+++ app/src/main/java/com/alessandro/pokeapp/BulbasaurActivity.java
@@ -0,0 +1,12 @@
+package com.alessandro.pokeapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BulbasaurActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bulbasaur);
+    }
+}
---
+++ app/src/main/java/com/alessandro/pokeapp/pokeapi/PokeapiService.java
@@ -0,0 +1,12 @@
+package com.alessandro.pokeapp.pokeapi;
+
+import com.alessandro.pokeapp.models.PokemonRespuesta;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PokeapiService {
+    @GET("pokemon")
+    Call<PokemonRespuesta> obtenerListaPokemon(@Query("limit") int limit, @Query("offset") int offset);
+}
